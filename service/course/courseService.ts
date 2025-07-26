96ff036b30ade70b8fea6785e7be62446ba848d1
mehedi <mehasan.softbd@gmail.com>
2025-07-26 20:07:02 +0600

    type error

--- service/course/courseService.ts
+++ service/course/courseService.ts
@@ -1,19 +1,23 @@
-import {apiDelete, apiPost, apiPut} from "../../common/api-route.ts/api-route.ts";
+import {
+  apiDelete,
+  apiPost,
+  apiPut,
+} from '../../common/api-route.ts/apiOperation';
 
 export const createCourse = async (data: any) => {
-    let response: any = await apiPost("", data);
-    return response.data;
+  let response: any = await apiPost('', data);
+  return response.data;
 };
 
 export const updateCourse = async (courseId: number, data: any) => {
-    let response: any = await apiPut("" + '/' + courseId, {
-        ...data,
-        updated_id: courseId,
-    });
-    return response.data;
+  let response: any = await apiPut('' + '/' + courseId, {
+    ...data,
+    updated_id: courseId,
+  });
+  return response.data;
 };
 
 export const deleteCourse = async (courseId: number) => {
-    let response: any = await apiDelete("" + '/' + courseId);
-    return response.data;
+  let response: any = await apiDelete('' + '/' + courseId);
+  return response.data;
 };
